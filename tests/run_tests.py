c2c40b3f19be857d500d2fbbac30eef7637a497d
Keith Bechtol <kbechtol@lsst.org>
2018-02-09 12:28:33 -0700

    Adding first test

---
+++ tests/run_tests.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+if __name__ == "__main__":
+    # Run all tests
+    np.testing.run_module_suite()
---
+++ tests/test_basic.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def test_basic():
+    """
+    Most basic test
+    """
+    a, b = 2, 2
+    x = a + b
+    np.assert_almost_equal(x, 4.)
+
+if __name__ == '__main__':
+    np.testing.run_module_suite()
