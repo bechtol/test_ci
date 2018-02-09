55ba42b5e17eef10eb0b28c640c4f0b8c0188ef3
Keith Bechtol <kbechtol@lsst.org>
2018-02-09 12:45:35 -0700

    Add a second unit test

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from test_basic import *
+
 if __name__ == "__main__":
     # Run all tests
     np.testing.run_module_suite()
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,20 @@
 import numpy as np
 
-def test_basic():
+def test_addition():
     """
-    Most basic test
+    Addition test
     """
     a, b = 2, 2
     x = a + b
     np.testing.assert_almost_equal(x, 4.)
+
+def test_multiplication():
+    """
+    Multiplication test
+    """
+    a, b = 3, 8
+    x = a * b
+    np.testing.assert_almost_equal(x, 24.)
 
 if __name__ == '__main__':
     np.testing.run_module_suite()
